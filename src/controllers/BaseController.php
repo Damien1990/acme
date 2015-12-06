9c58f31fd1e28e322fc4291d2758140e06abc645
Damien McKeever <damienmckeever@hotmail.co.uk>
2015-12-06 23:33:50 +0000

    Initial commit

---
+++ src/controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acme\controllers;
+
+use duncan3dc\Laravel\BladeInstance;
+
+class BaseController
+{
+    protected $blade;
+
+    public function __construct()
+    {
+        $this->blade = new BladeInstance("/vagrant/views", "/vagrant/cache/views");
+    }
+}
